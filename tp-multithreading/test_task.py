f4fbcc05c5de504b2b5c799e66e8879fc5bc9fd1
SoumaGhazouani <119610492+SoumaGhazouani@users.noreply.github.com>
2024-11-20 15:30:19 +0100

    Create test_task.py

---
+++ tp-multithreading/test_task.py
@@ -0,0 +1,15 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+from task import Task  
+
+class TestTask(unittest.TestCase):
+    def test_solve_ax_equals_b(self):
+        task = Task(size=5)  
+      
+        task.work()
+        
+        npt.assert_allclose(np.dot(task.a, task.x), task.b, rtol=1e-5, atol=1e-8)
+
+if __name__ == "__main__":
+    unittest.main()
